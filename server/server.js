8385848c019167c3e7869979a7d8746d7f86dd70
Chala Temesgen <ctemesgen85@gmail.com>
2025-11-17 18:41:36 +0300

    SSE endpoint for clients added

--- server/server.js
+++ server/server.js
@@ -4,13 +4,20 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 import connectDB from './configs/db.js';
-//import sseRoutes from "./routes/sseRoutes.js";
+import { sseHandler } from './sse/sseManager.js';
+
+
 
 connectDB();
 
 const app = express();
 app.use(cors());
-//app.use("/api", sseRoutes);
+app.use(express.json());
+
+
+// SSE endpoint for clients
+app.get("/api/events", sseHandler);
+
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
