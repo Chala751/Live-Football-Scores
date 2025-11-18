79be7618c64d335f8c0368277054ea342a45835e
Chala Temesgen <ctemesgen85@gmail.com>
2025-11-18 20:41:21 +0300

    Create new match added

---
+++ server/controllers/gameController.js
@@ -0,0 +1,13 @@
+import Game from "../models/Game.js";
+import { broadcast } from "../sse/sseManager.js";
+
+// Create new match
+export const createGame = async (req, res) => {
+  try {
+    const game = await Game.create(req.body);
+    broadcast("newGame", game);
+    res.json(game);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
