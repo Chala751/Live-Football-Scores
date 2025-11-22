99c139d1475aae9d069f1de28089f4b196a5d850
Chala Temesgen <ctemesgen85@gmail.com>
2025-11-22 21:06:29 +0300

    CreateGame added

---
+++ client/src/pages/CreateGame.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { createGame } from "../api/api";
+
+export default function CreateGame() {
+  const [homeTeam, setHomeTeam] = useState("");
+  const [awayTeam, setAwayTeam] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await createGame({ homeTeam, awayTeam });
+    alert("Game created");
+    setHomeTeam("");
+    setAwayTeam("");
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Create New Game</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Home team"
+          value={homeTeam}
+          onChange={(e) => setHomeTeam(e.target.value)}
+        /><br />
+
+        <input
+          placeholder="Away team"
+          value={awayTeam}
+          onChange={(e) => setAwayTeam(e.target.value)}
+        /><br />
+
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
+}
