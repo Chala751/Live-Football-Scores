cdaa23728a8e9589d3400c8c8072bcd148adde30
Chala Temesgen <ctemesgen85@gmail.com>
2025-11-25 15:50:46 +0300

    update score updated

--- client/src/pages/UpdateScore.jsx
+++ client/src/pages/UpdateScore.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { fetchGame, updateScore } from "../services/api";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom"; 
 
 export default function UpdateScore() {
   const { id } = useParams();
@@ -32,18 +33,22 @@ export default function UpdateScore() {
       <form onSubmit={handleSubmit}>
         <input
           type="number"
+          min={0}
           value={home}
           onChange={(e) => setHome(Number(e.target.value))}
         />
         <span> - </span>
         <input
           type="number"
+          min={0}
           value={away}
           onChange={(e) => setAway(Number(e.target.value))}
         />
 
         <button>Save Score</button>
       </form>
+       <br />
+      <Link to="/">← Back to Home</Link> 
     </div>
   );
 }
